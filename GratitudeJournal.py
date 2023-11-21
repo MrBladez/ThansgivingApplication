467f379776a84a010fcecb85024c37a2a05df76e
MrBladez <austin.minute@gmail.com>
2023-11-20 22:52:34 -0600

    Added While Loop for User Error

--- GratitudeJournal.py
+++ GratitudeJournal.py
@@ -7,6 +7,8 @@ file = open("Journal.txt")
 while True:
     userChoice = input("Would you like to enter or view a journal entry or exit? ")
     if userChoice == "enter" or userChoice == "Enter":
+        if input("Would you like to go back? (yes/no) ") == "yes":
+            continue
         userEntry = input("What would you like to enter? ")
         print(userEntry, file=open('Journal.txt', 'a'))
         print("EndOfEntry", file=open('Journal.txt', 'a'))
@@ -21,8 +23,14 @@ while True:
                     entry = ""
                 else:
                     entry += item + " "
-        whichToView = int(input("What entry would you like to see? (" + str(EntryNumber) + " possible entries) "))
-        print("This is entry number " + str(whichToView) + " : " + entryList[whichToView-1])
+        while True:
+            if input("Would you like to go back? (yes/no) ") == "yes":
+                break
+            whichToView = int(input("What entry would you like to see? (" + str(EntryNumber) + " possible entries) "))
+            if whichToView <= EntryNumber and whichToView > 0:
+                print("This is entry number " + str(whichToView) + " : " + entryList[whichToView-1])
+            else:
+                print("That numbered entry does not exist at the moment.")
     elif userChoice == "Exit" or userChoice == "exit":
         break
 
